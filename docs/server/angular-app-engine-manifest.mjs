c4a53fe6b8057f2aa55dd5085821b8a359a45590
AlexChuquipoma <alexrdfchuquipomavs@gmail.com>
2025-11-16 22:12:36 -0500

    Build Angular listo para GitHub Pages

---
+++ docs/server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: '/icc-ppw-u2-01Fundamentos',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
---
+++ docs/server/assets-chunks/index_html.mjs
@@ -0,0 +1,13 @@
+export default `<!DOCTYPE html><html lang="en" data-beasties-container><head>
+  <meta charset="utf-8">
+  <title>MiAppAngular</title>
+  <base href="/icc-ppw-u2-01Fundamentos/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+<link rel="stylesheet" href="styles-5INURTSO.css"><style ng-app-id="ng">nav[_ngcontent-ng-c584486]{background-color:#159;padding:1rem;overflow:hidden}a[_ngcontent-ng-c584486]{color:#fff;text-decoration:none;margin-right:1rem;font-weight:700}a.active[_ngcontent-ng-c584486]{color:#ff0;text-decoration:underline}</style></head>
+<body><!--nghm--><script type="text/javascript" id="ng-event-dispatch-contract">(()=>{function p(t,n,r,o,e,i,f,m){return{eventType:t,event:n,targetElement:r,eic:o,timeStamp:e,eia:i,eirp:f,eiack:m}}function u(t){let n=[],r=e=>{n.push(e)};return{c:t,q:n,et:[],etc:[],d:r,h:e=>{r(p(e.type,e,e.target,t,Date.now()))}}}function s(t,n,r){for(let o=0;o<n.length;o++){let e=n[o];(r?t.etc:t.et).push(e),t.c.addEventListener(e,t.h,r)}}function c(t,n,r,o,e=window){let i=u(t);e._ejsas||(e._ejsas={}),e._ejsas[n]=i,s(i,r),s(i,o,!0)}window.__jsaction_bootstrap=c;})();
+</script><script>window.__jsaction_bootstrap(document.body,"ng",["click"],[]);</script>
+  <app-root ng-version="20.3.9" ngh="1" ng-server-context="ssg"><app-nav-bar _nghost-ng-c584486 ngh="0"><nav _ngcontent-ng-c584486><a _ngcontent-ng-c584486 routerlink routerlinkactive="active" href="/icc-ppw-u2-01Fundamentos/" class="active" jsaction="click:;">Home</a><a _ngcontent-ng-c584486 routerlink="/perfil" routerlinkactive="active" href="/icc-ppw-u2-01Fundamentos/perfil" class jsaction="click:;">Perfil</a><a _ngcontent-ng-c584486 routerlink="/proyectos" routerlinkactive="active" href="/icc-ppw-u2-01Fundamentos/proyectos" class jsaction="click:;">Proyectos</a><a _ngcontent-ng-c584486 routerlink="/proyectos-dos" routerlinkactive="active" href="/icc-ppw-u2-01Fundamentos/proyectos-dos" class jsaction="click:;">Proyectos Dos</a><a _ngcontent-ng-c584486 routerlink="/formulariopage" routerlinkactive="active" href="/icc-ppw-u2-01Fundamentos/formulariopage" class jsaction="click:;">Formulario</a></nav></app-nav-bar><router-outlet></router-outlet><app-homepage ngh="0"><h1>Home</h1><h2>Fundamentos</h2><h1>Contador (Variable Normal): 0</h1><h1>Contador (Signal): 0</h1><button jsaction="click:;">Incrementar</button><button jsaction="click:;">Decrementar</button><button jsaction="click:;">Reset</button></app-homepage><!----></app-root>
+<script src="main-YY6O4WHQ.js" type="module"></script>
+
+<script id="ng-state" type="application/json">{"__nghData__":[{},{"c":{"1":[{"i":"c1107669026","r":1}]}}]}</script></body></html>`;
